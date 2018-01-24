bbf82d670c71fec9f549494390f1a6007261e8c3
zagyar <zagyar@gmail.com>
2018-01-24 16:07:00 +0200

    Added initail files

---
+++ src/RequestService/RequestConsumer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RequestService
+{
+    using System.Threading.Tasks;
+    using MassTransit;
+    using MassTransit.Logging;
+    using Sample.MessageTypes;
+
+
+    public class RequestConsumer :
+        IConsumer<ISimpleRequest>
+    {
+        readonly ILog _log = Logger.Get<RequestConsumer>();
+		
+        public async Task Consume(ConsumeContext<ISimpleRequest> context)
+        {
+            _log.InfoFormat("Returning name for {0}", context.Message.CustomerId);
+	        await Task.Run(() => throw new AggregateException());
+			//same as throw new  AggregateException();
+        }
+    }
+}
